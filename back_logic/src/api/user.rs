d975f97f3401e6b7007a3cc584b0aeb148a0d97e
Yuki-iso <user.yukisnow@gmail.com>
2024-02-28 13:35:53 +0100

    Non functional setting up endpoint for biscuit

--- back_logic/src/api/user.rs
+++ back_logic/src/api/user.rs
@@ -38,3 +38,35 @@ async fn create_user(db: web::Data<Database>, form: web::Form<User>) -> HttpResp
     }
 }
 
+#[derive(Deserialize)]
+pub struct UserLogin{
+    user_name: String,
+    password: String
+}
+
+#[post("/login")]
+async fn login(db: web::Data<Database>, credentials: web::Json<UserLogin> ) -> HttpResponse {
+    let collection = db.collection(COL_NAME);
+    let result = collection.insert_one(form.into_inner(), None).await;
+    match result {
+        Ok(_) => HttpResponse::Ok().body("user added"),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+
+
+    let collection: Collection<User> = db.collection(COL_NAME);
+
+    match collection
+        .find_one(doc! { "username": credentials.username }, None)
+        .await
+    {
+        Ok(Some(user)) => HttpResponse::Ok().json(user),
+        Ok(None) => {
+            HttpResponse::NotFound().body(format!("No user found with username {username}"))
+        }
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+
+    
+
+}
