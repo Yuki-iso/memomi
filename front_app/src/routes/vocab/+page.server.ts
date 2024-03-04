8d774feac33b375a19a48e6b26e5d97d450a9209
Jeffrey van der Zande <jeffrey.van.der.zande@hva.nl>
2024-03-04 13:09:31 +0100

    update: flashcard system

--- front_app/src/routes/vocab/+page.server.ts
+++ front_app/src/routes/vocab/+page.server.ts
@@ -1,15 +1,47 @@
-export async function load(){
-  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/new_words/65cb371d3390aaa918373c02`, {
+export async function load() {
+	/*const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/new_words/65cb371d3390aaa918373c02`, {
     method: "GET",
     headers: {
       'Content-Type': 'application/json',
     },
+  }*/
+
+	//const data = await res.json();
+	const cards = [
+		{
+			kanji: '〜か月',
+			jp_sentence: '日本[にほん]に 来[き]ました。',
+			english: '3 months ago',
+			en_sentence: 'I2 came to Japan',
+			status: 'New'
+		},
+		{
+			kanji: '〜か月2',
+			jp_sentence: '日本[にほん]に 来[き]ました。',
+			english: '2 months ago',
+			en_sentence: 'I came to Japan',
+			status: 'Review'
+		},
+		{
+			kanji: '〜か月3',
+			jp_sentence: '日本[にほん]に 来[き]ました。',
+			english: '1 month ago',
+			en_sentence: 'I came to Japan',
+			status: 'New'
+		}
+	];
+	return {
+		props: cards
+	};
+}
+
+export async function update(id: number, status: string){
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/update_status`, {
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({id: id, status: status}),
   });
-  const data = await res.json();
-  console.log(data);
-  return {
-    props: {
-      data
-    }
-  }
+  return res.json();
 }
--- front_app/src/routes/vocab/+page.svelte
+++ front_app/src/routes/vocab/+page.svelte
@@ -1,16 +1,34 @@
 <script>
-  export let data;
-  let state = true;
-  let progress = 30;
+	export let data;
+	let cards = data.props;
+	let show_card = false;
+	let progress = 0;
+	let id = 0;
+	let max_progress = cards.length;
 
-  let front_card = {
-    kanji: "〜か月",
-    sentence: "日本[にほん]に 来[き]ました。",
-  }
-  let back_card = {
-    english: "2 months ago",
-    sentence: "I came to Japan",
-  }
+	$: new_cards = cards.filter((card) => card.status === 'New').length;
+	$: failed_cards = cards.filter((card) => card.status === 'Failed').length;
+	$: review_cards = cards.filter((card) => card.status === 'Review').length;
+	let card = cards[id];
+
+	function next_card(event) {
+		switch (event.target.innerText) {
+			case 'Easy':
+				progress += 100 / max_progress;
+				cards = cards.filter((_, index) => index !== id); 
+				break;
+			case 'Medium':
+				progress += 100 / max_progress;
+				cards = cards.filter((_, index) => index !== id); 
+				break;
+			case 'Hard':
+				cards[id].status = 'Failed';
+
+				break;
+		}
+		card = cards[id];
+		show_card = false;
+	}
 </script>
 
 <svelte:head>
@@ -19,27 +37,53 @@
 </svelte:head>
 
 <div class="flex justify-center w-screen h-full pt-20">
-	<div class="flex flex-col justify-center w-1/2">
-    <div class="h-6 rounded-xl border-gray-400 border-2 my-4">
-      <div style="width: {progress}%" class="bg-green-500 h-full rounded-xl"></div>
-    </div>
-		<div class="bg-gray-100 text-center flex flex-col justify-center rounded-xl h-[600px]">
-      {#if state}
-			<p class="text-5xl font-bold p-5">{front_card.kanji}</p>
-			<p class="text-2xl p-5">{front_card.sentence}</p>
-      {:else}
-      <p class="text-5xl font-bold p-5">{back_card.english}</p>
-      <p class="text-2xl p-5">{back_card.sentence}</p>
-      {/if}
+	{#if card != null}
+		<div class="flex flex-col justify-center w-1/2">
+			<div class="h-6 rounded-xl border-gray-400 border-2 my-4">
+				<div style="width: {progress}%" class="bg-green-500 h-full rounded-xl"></div>
+			</div>
+			<div class="bg-gray-100 text-center flex flex-col justify-center rounded-xl h-[600px]">
+				{#if show_card}
+					<p class="text-5xl font-bold p-5">{card.english}</p>
+					<p class="text-2xl p-5">{card.en_sentence}</p>
+				{:else}
+					<p class="text-5xl font-bold p-5">{card.kanji}</p>
+					<p class="text-2xl p-5">{card.jp_sentence}</p>
+				{/if}
+			</div>
+			<div class="flex gap-4 justify-center">
+				{#if !show_card}
+					<button
+						on:click={() => (show_card = !show_card)}
+						class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100">Show</button
+					>
+				{:else}
+					<button
+						on:click={(event) => next_card(event)}
+						class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100 text-green-500"
+						>Easy</button
+					>
+					<button
+						on:click={(event) => next_card(event)}
+						class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100 text-yellow-500"
+						>Medium</button
+					>
+					<button
+						on:click={(event) => next_card(event)}
+						class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100 text-red-500"
+						>Hard</button
+					>
+				{/if}
+			</div>
+			{#key new_cards}
+				<div class="flex justify-center text-center gap-4">
+					<p class="text-blue-300">{new_cards}</p>
+					<p class="text-red-300">{failed_cards}</p>
+					<p class="text-green-300">{review_cards}</p>
+				</div>
+			{/key}
 		</div>
-		<div class="flex gap-4 justify-center">
-      {#if state}
-			<button on:click={() => state = !state} class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100">Show</button>
-      {:else}
-      <button on:click={() => state = !state} class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100 text-green-500">Easy</button>
-      <button on:click={() => progress += 10} class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100 text-yellow-500">Medium</button>
-      <button on:click={() => state = !state} class="p-5 w-1/4 my-4 rounded-xl text-xl font-bold bg-gray-100 text-red-500">Hard</button>
-      {/if}
-		</div>
-	</div>
+	{:else}
+		<p class="text-5xl font-bold p-5">No more cards</p>
+	{/if}
 </div>
